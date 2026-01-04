3724fc0266494b50461c0595331eeea25ea4bfaf
gfianu <delyouandme@gmail.com>
2026-01-04 07:17:07 -0500

    age calculator test complete

--- src/test/java/org/example/AgeCalculatorTest.java
+++ src/test/java/org/example/AgeCalculatorTest.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AgeCalculatorTest {
+    @Test
+    public void testYearsUntil100Typical() {
+        assertEquals(70, AgeCalculator.yearsUntil100(30));
+    }
+
+    @Test
+    public void testYearsUntil100Already100() {
+        assertEquals(0, AgeCalculator.yearsUntil100(100));
+    }
+
+    @Test
+    public void testYearsUntil100Over100() {
+        assertEquals(-5, AgeCalculator.yearsUntil100(105));
+    }
 }
